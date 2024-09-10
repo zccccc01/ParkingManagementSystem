be9bac7b6aab0dacfbb86735d687f4f134fc151c
zccccc01 <1351688749@qq.com>
2024-09-10 16:06:40 +0800

    fixed(/repository/parking_space_repository.go) update func

--- backend/internal/repository/parking_space_repository.go
+++ backend/internal/repository/parking_space_repository.go
@@ -5,12 +5,12 @@ import "github.com/zccccc01/ParkingManagementSystem/backend/internal/models"
 type ParkingSpaceRepository interface {
 	// 创建一条记录
 	Create(space *models.ParkingSpace) error
-	// 根据车位id获取一条记录
+	// 根据车位id获取停车场id记录,TODO:应该是从LotID连表查spaceID
 	GetLotIDBySpaceID(id int) (int, error)
 	// 根据车位id获取状态
 	GetStatusBySpaceID(id int) (string, error)
-	// 根据车位id获取一条记录
+	// 根据车位id更新状态
 	UpdateStatusBySpaceID(space *models.ParkingSpace, id int) error
-	// 根据车位id获取一条记录
+	// 根据车位id删除一条记录
 	Delete(id int) error
 }
--- backend/internal/repository/parking_space_repository_impl.go
+++ backend/internal/repository/parking_space_repository_impl.go
@@ -1,1 +1,50 @@
 package repository
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/zccccc01/ParkingManagementSystem/backend/internal/models"
+)
+
+type ParkingSpaceRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewParkingSpaceRepository(db *gorm.DB) ParkingRecordRepository {
+	return &ParkingSpaceRepositoryImpl{DB: db}
+}
+
+func (r *ParkingSpaceRepositoryImpl) Create(space *models.ParkingSpace) error {
+	return r.DB.Create(space).Error
+}
+
+func (r *ParkingSpaceRepositoryImpl) GetLotIDBySpaceID(id int) (int, error) {
+	var space models.ParkingSpace
+	result := r.DB.First(&space, "SpaceID = ?", id)
+	if result.Error != nil {
+		return -1, result.Error
+	}
+	if result.RowsAffected == 0 {
+		return -1, gorm.ErrRecordNotFound
+	}
+	return space.ParkingLotID, nil
+}
+
+func (r *ParkingSpaceRepositoryImpl) GetStatusBySpaceID(id int) (string, error) {
+	var space models.ParkingSpace
+	result := r.DB.First(&space, "SpaceID = id", id)
+	if result.Error != nil {
+		return "", result.Error
+	}
+	if result.RowsAffected == 0 {
+		return "", gorm.ErrRecordNotFound
+	}
+	return space.Status, nil
+}
+
+func (r *ParkingSpaceRepositoryImpl) UpdateStatusBySpaceID(space *models.ParkingSpace, id int) error {
+	return nil
+}
+
+func (r *ParkingSpaceRepositoryImpl) Delete(id int) error {
+	return nil
+}
