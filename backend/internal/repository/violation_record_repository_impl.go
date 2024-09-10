2a7ba3f14698f7e3afdbe28083a1c71575aa301d
956 <2780428016@qq.com>
2024-09-10 20:56:05 +0800

    956 violation

--- backend/internal/repository/violation_record_repository_impl.go
+++ backend/internal/repository/violation_record_repository_impl.go
@@ -1,1 +1,38 @@
 package repository
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/zccccc01/ParkingManagementSystem/backend/internal/models"
+)
+
+type ViolationRecordRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func (r *ViolationRecordRepositoryImpl) Create(violation *models.ViolationRecord) error {
+	return r.DB.Create(violation).Error
+}
+func (r *ViolationRecordRepositoryImpl) GetFineAmountByRecordID(id int) (float64, error) {
+	var violationRecord models.ViolationRecord
+	result := r.DB.Where("record_id = ?", id).First(&violationRecord)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+	return violationRecord.FineAmount, nil
+}
+func (r *ViolationRecordRepositoryImpl) GetStatusByRecordID(id int) (string, error) {
+	var violationRecord models.ViolationRecord
+	result := r.DB.Where("record_id = ?", id).First(&violationRecord)
+	if result.Error != nil {
+		return "", result.Error
+	}
+	return violationRecord.Status, nil
+}
+func (r *ViolationRecordRepositoryImpl) GetViolationTypeByRecordID(id int) (string, error) {
+	var violationRecord models.ViolationRecord
+	result := r.DB.Where("record_id = ?", id).First(&violationRecord)
+	if result.Error != nil {
+		return "", result.Error
+	}
+	return violationRecord.ViolationType, nil
+}
