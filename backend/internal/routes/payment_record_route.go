c9a2b64e03108707a8e6c2c435d83c3ffef6b73d
zccccc01 <1351688749@qq.com>
2024-10-08 14:01:54 +0800

    fix record-route

--- backend/internal/routes/payment_record_route.go
+++ backend/internal/routes/payment_record_route.go
@@ -1,1 +1,25 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	"github.com/zccccc01/ParkingManagementSystem/backend/internal/controllers"
+	"github.com/zccccc01/ParkingManagementSystem/backend/internal/repository"
+)
+
+func SetupPaymentRecordRoutes(app *fiber.App, db *gorm.DB) {
+	// 初始化 repository 和 service
+	paymentRepo := repository.NewParkingSpaceRepository(db)
+	parkingSpaceController := controllers.NewParkingSpaceController(paymentRepo)
+
+	// 定义路由组
+	paymentRecord := app.Group("/api/paymentrecord")
+
+	// 定义路由
+	paymentRecord.Get("/vehicle/:plateNumber", parkingSpaceController.GetParkingSpaceByLicensePlate)
+	paymentRecord.Get("/user/:id", parkingSpaceController.GetParkingSpaceByUserID)
+	paymentRecord.Get("/lot/:id", parkingSpaceController.GetParkingSpaceByParkingLotId)
+	paymentRecord.Get("/status/lot/:lotid/space/:spaceid", parkingSpaceController.GetParkingSpaceStatusById) //TODO:传两个参数
+	paymentRecord.Post("/", parkingSpaceController.CreateParkingSpace)
+	paymentRecord.Put("/status/lot/:lotid/space/:spaceid", parkingSpaceController.UpdateParkingSpaceStatus) //TODO:传两个参数
+}
