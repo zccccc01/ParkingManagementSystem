54233632ac96e779a9c0ee3c5e0475c91628a3f2
956 <2780428016@qq.com>
2024-10-10 13:56:29 +0800

    fix GetStatusByLotIDAndSpaceID

--- backend/internal/repository/parking_space_repository.go
+++ backend/internal/repository/parking_space_repository.go
@@ -8,7 +8,7 @@ type ParkingSpaceRepository interface {
 	// 根据停车场id获取该停车场车位空余情况
 	GetAllStatusByLotID(id int) ([]models.ParkingSpace, error)
 	// 根据车位id获取状态 TODO:(前提是知道LotID),需要修改/删除
-	GetStatusBySpaceID(id int) (string, error)
+	GetStatusByLotIDAndSpaceID(l_id int, s_id int) (string, error)
 	// 根据车位id更新状态 TODO:两个参数lotID,spaceID
 	UpdateStatusBySpaceID(space *models.ParkingSpace, id int) (bool, error)
 	// 根据车牌号查看停车位置(LotID,SpaceID) TODO:想改一下返回值
